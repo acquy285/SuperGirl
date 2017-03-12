307baf54eeb875d69d653887b82d5bc63432bbca
MorgothGatts <hellblackcat1408@gmail.com>
2017-03-12 20:35:46 +0700

    TCP Server-Client

---
+++ Source Code/Server-Client/Simple TCP Server - Client/TCPClient.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPClient {
+	public static void main(String argv[]) throws Exception {
+		
+		//Declaration Section
+		String sentence;
+		String modifiedSentence;
+		
+		//----------------------------------------------------------------------------------
+		
+		while(true) {
+			Socket clientSocket = new Socket("localhost", 4444);
+			BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		//----------------------------------------------------------------------------------
+		
+			System.out.println("Message to Server: ");
+			sentence = inFromUser.readLine();
+			outToServer.writeBytes(sentence + '\n');
+			modifiedSentence = inFromServer.readLine();
+			System.out.println("FROM SERVER: " + modifiedSentence);
+		}
+	}
+}
